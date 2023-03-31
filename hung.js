97bf83df37cfc5d5e12235f88df6469834e1dbae
kina1704 <66508720+kina1704@users.noreply.github.com>
2023-03-31 17:13:52 +0900

    Update hung.js

--- hung.js
+++ hung.js
@@ -1,6 +1,1 @@
-  var body = document.querySelector('body');
-  body.innerHTML = '';
-  var newContent = document.createElement('p');
-  var text = document.createTextNode('Chào bạn, mình đã thay đổi body của web');
-  newContent.appendChild(text);
-  body.appendChild(newContent);
+alert("Cảnh đẹp zai")
